8e95e848665055086c3206d0a1621e80731a32f4
Harish <nharishreddy077@gmail.com>
2024-12-10 09:19:27 +0530

    update

--- assignment4.c
+++ assignment4.c
@@ -1,18 +1,18 @@
 // floyds triangle
-// #include<stdio.h>
-// int main(){
-//     int i,j,rows,n=1;
-//     printf("enter the number of rows: ");
-//     scanf("%d",&rows);
-//     for(i=1;i<rows;i++){
-//         for(j=1;j<=i;j++){
-//             printf("%d\t",n);
-//             n++;
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
+#include<stdio.h>
+int main(){
+    int i,j,rows,n=1;
+    printf("enter the number of rows: ");
+    scanf("%d",&rows);
+    for(i=1;i<rows;i++){
+        for(j=1;j<=i;j++){
+            printf("%d\t",n);
+            n++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 
 // sand glass pattern
